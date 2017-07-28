30f271e627a6690d3cc3e66c6cb5822d523e9e97
Scott Bromander <scott@primeacademy.io>
2017-07-28 15:37:07 -0500

    added speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float speed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,16 +14,16 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.W)) {
-			gameObject.transform.Translate (transform.forward * Time.deltaTime);
+			gameObject.transform.Translate (transform.forward * Time.deltaTime * speed);
 		} 
 		if (Input.GetKey (KeyCode.S)) {
-			gameObject.transform.Translate (-transform.forward * Time.deltaTime);
+			gameObject.transform.Translate (-transform.forward * Time.deltaTime * speed);
 		} 
 		if (Input.GetKey (KeyCode.A)) {
-			gameObject.transform.Translate (-transform.right * Time.deltaTime);
+			gameObject.transform.Translate (-transform.right * Time.deltaTime * speed);
 		} 
 		if (Input.GetKey (KeyCode.D)) {
-			gameObject.transform.Translate (transform.right * Time.deltaTime);
+			gameObject.transform.Translate (transform.right * Time.deltaTime * speed);
 		} 
 	}
 }
